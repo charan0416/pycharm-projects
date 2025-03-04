4282abfe25faca91be9509f6438dbddf0d4c80f3
charan0416 <charanbaru169@gmail.com>
2025-03-04 17:11:02 +0530

    loopps

--- loops.py
+++ loops.py
@@ -1,27 +1,56 @@
-i=0
-while i < 10:
-    i+=1
-    print("*"*i)
-num =12345
-fact =1
-while num > 0:
-    fact *= num%10
-    num = num//10
-print(fact)
+# i=0
+# while i < 10:
+#     i+=1
+#     print("*"*i)
+# num =12345
+# fact =1
+# while num > 0:
+#     fact *= num%10
+#     num = num//10
+# print(fact)
+#
+#
+# num = 54633
+# sum=0
+# while num > 0:
+#     sum=sum+num%10
+#     num=num//10
+# print(sum)
+#
+#
+# l=[1,2,3,4,5,6,7,8,9,10]
+# for item in l:
+#     if item%5 ==0:
+#         break
+#         print(items)
+#     else:
+#         print("loop exit successfully")
 
 
-num = 54633
-sum=0
-while num > 0:
-    sum=sum+num%10
-    num=num//10
-print(sum)
+#
+# i = 1
+# while i < 11:
+#     j=1
+#     while j< 11:
+#         product = i*j
+#         print(f"{i}*{j} = {i*j}")
+#     print("\n")
+# fruits = ["apple","orange","banana"]
+# persons = ["ramesh","suresh","paresh"]
+# for fruit in fruits:
+#     for person in persons:
+#         print(f"1{fruit} to {person}")
+#     print("\n")
+#     for i in range(1, 11):
+#        for j in range(1, 11):
+#         print(f"{i} * {j} = {i*j}")
+#         if j%5==0:
+#             break
+#     print()
+#     if i%3==0:
+#         break
 
-
-l=[1,2,3,4,5,6,7,8,9,10]
-for item in l:
-    if item%5 ==0:
-        break
-        print(items)
-    else:
-        print("loop exit successfully")
+#
+# def fucntion ():
+#     if i in range(1,100):
+#         pass
